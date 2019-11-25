9efae59a888a3872d5b1014b12aed8abc4fe2c2b
Abdulsalam Shahlol <abdulsalamshahlol@gmail.com>
2019-11-25 09:17:06 +0100

    done but needs refactor

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -7,5 +7,29 @@
  */
 
 var firstNonRepeatedCharacter = function(string) {
-  // TODO: your solution here
+    var strArr = Array.from(string);
+    var count = 0;
+    var res = '';
+    var mapped = [];
+
+    for (var j = 0; j < strArr.length; j++) {
+        var c = 0;
+        for (var i = 0; i < strArr.length; i++) {
+            if (strArr[i] === strArr[j]) {
+                c++;
+            }
+        }
+        mapped.push({
+            char: strArr[j],
+            count: c
+        });
+    }
+
+    res = mapped.filter(function(elem, index) {
+        if (elem.count === 1) {
+            return elem;
+        }
+    })
+
+    return res[0].char
 };
