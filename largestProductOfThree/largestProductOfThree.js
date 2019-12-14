1134e12479e87551eafb9e6a517c60be96419dce
Abdulsalam Shahlol <abdulsalamshahlol@gmail.com>
2019-12-14 09:44:57 +0100

    solved Largest product of three. Forgot to pull from master to update QueryString problem

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -7,4 +7,23 @@ largestProductOfThree([ 0, 2, 3 ]) //	0
 largestProductOfThree([1, 4, -5, -2]) // 40
 */
 
-function largestProductOfThree(array) {}
+console.clear();
+function largestProductOfThree(array) {
+    array = array.map(n=>Math.abs(n)).sort((a,b)=>a - b);
+    let largest = array[array.length - 1];
+    let smallest = array[0];
+    let nums = [];
+
+    for (var c = array.length - 1; c >= 0; c--) {
+        if (array[c] <= largest && nums.length < 3) {
+            nums.push(array[c]);
+        }
+    }
+    return nums.reduce((acc,n)=>acc * n);
+}
+
+// console.log(largestProductOfThree([2, 7, 3, 1, 4]));
+// console.log(largestProductOfThree([2, 1, 3, 7]));
+// console.log(largestProductOfThree([0, 2, 3]));
+// console.log(largestProductOfThree([1, 4, -5, -2]));
+// console.log(largestProductOfThree([ 0, 2, 7, 1, 5 ]));
