56f5cf536c0bb1412b83f7898c28a91915f6313b
Abdulsalam Shahlol <abdulsalamshahlol@gmail.com>
2019-12-28 09:34:59 +0100

    solved

--- evenOccurrence/evenOccurrence.js
+++ evenOccurrence/evenOccurrence.js
@@ -7,3 +7,29 @@ evenOccurrence([ "cat", "dog", "dig", "cat" ]) // "cat"
 evenOccurrence([[1,2,3], [1,2], [1,2,3]]) // [1,2,3]
 evenOccurrence([{a: 1, b: 2}, {a:1, b:3}, {a:1, b:2}]) // {a:1, b:2}
 */
+
+var evenOccurrence = function (arr) {
+    let counterObj = {};
+    let result = null;
+
+    arr.forEach((i) => {
+        let key = JSON.stringify(i);
+        if (!counterObj[key]) {
+            counterObj[key] = 1;
+        } else {
+            counterObj[key] = counterObj[key] + 1;
+        }
+    });
+
+    for (let i in counterObj) {
+        if (counterObj[i] % 2 === 0) {
+            try {
+                result = JSON.parse(i);
+            } catch (e) {
+                result = i;
+            }
+            break;
+        }
+    }
+    return result;
+}
