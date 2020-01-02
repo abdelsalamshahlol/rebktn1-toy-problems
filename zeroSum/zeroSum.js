ddf6ed2769d76e08e076587b34d8904154e71ea1
Abdulsalam Shahlol <abdulsalamshahlol@gmail.com>
2020-01-02 22:41:09 +0100

    solved

--- zeroSum/zeroSum.js
+++ zeroSum/zeroSum.js
@@ -7,3 +7,16 @@ Constraints: Solve this linear time complexity O(n)
 zeroSum([ 1, 3, 2, -3 ]) // true
 zeroSum([ 5, 7, 2, 9 ]) // false
 */
+
+let zeroSum = function (arr) {
+    let ob = {};
+    for (let i = 0; i < arr.length; i++) {
+        if (!ob[arr[i]]) {
+            ob[-arr[i]] = -arr[i];
+        }
+        if (ob[arr[i]]) {
+            return true;
+        }
+    }
+    return false;
+};
