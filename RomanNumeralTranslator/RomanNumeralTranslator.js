57b5e253b96d8f44cc145b950009dec9b3b9d782
Abdelsalam Shahlol <abdulsalamshahlol@gmail.com>
2020-02-20 10:03:22 +0100

    solved roman numeral

--- RomanNumeralTranslator/RomanNumeralTranslator.js
+++ RomanNumeralTranslator/RomanNumeralTranslator.js
@@ -1,6 +1,7 @@
 // Given a roman numeral as input, write a function that converts the roman numeral to a number and outputs it.
 
-// In a roman numeral, each character adds its value to the total. See the helper object DIGIT_VALUES for the correspondence between roman numeral characters and integers.
+// In a roman numeral, each character adds its value to the total.
+// See the helper object DIGIT_VALUES for the correspondence between roman numeral characters and integers.
 // VI = 6 (5 + 1 = 6)
 // LXX = 70 (50 + 10 + 10 = 70)
 // MCC = 1200 (1000 + 100 + 100 = 1200)
@@ -14,17 +15,27 @@
 
 // You've got Helpers!
 
-// var DIGIT_VALUES = {
-//     I: 1,
-//     V: 5,
-//     X: 10,
-//     L: 50, 
-//     C: 100,
-//     D: 500,
-//     M: 1000
-//   };
+console.clear();
+let DIGIT_VALUES = {
+  V: 5,
+  I: 1,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000
+};
 
-function translateRomanNumeral (romanNumeral) {
-    // Write your code here, and
-    // return your final answer.
+function translateRomanNumeral(romanNumeral) {
+  let split = romanNumeral.split('')
+  if (!DIGIT_VALUES[split[0]])
+    return null;
+  if (DIGIT_VALUES[split[0]] > DIGIT_VALUES[split[1]]) {
+    return split.reduce((memo,curr)=>{
+          return memo + DIGIT_VALUES[curr];
+        }
+        , 0);
+  } else {
+    return DIGIT_VALUES[split[1]] - DIGIT_VALUES[split[0]];
   }
+}
